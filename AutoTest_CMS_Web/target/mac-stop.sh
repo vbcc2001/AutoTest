173f2f1ae51ce90fa5508b3f2c846849d175714b
vbcc2001 <vbcc2001@gmail.com>
2017-08-04 20:31:55 +0800

    77
    77

--- AutoTest_CMS_Web/target/mac-stop.sh
+++ AutoTest_CMS_Web/target/mac-stop.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/java -Dfile.encoding=UTF-8 -Dapp.home=/Users/tuzi/AndroidStudioProjects/AutoTest/AutoTest_CMS_Web/target/webapp -Dapp.name=AutoTest_CMS_Web -Dapp.port=3000 -jar /Users/tuzi/AndroidStudioProjects/AutoTest/AutoTest_CMS_Web/target/resin-pro-3.1.12/lib/resin.jar -resin-home /Users/tuzi/AndroidStudioProjects/AutoTest/AutoTest_CMS_Web/target/resin-pro-3.1.12 -conf /Users/tuzi/AndroidStudioProjects/AutoTest/AutoTest_CMS_Web/target/resin.xml -server AutoTest_CMS_Web  stop
--- AutoTest_CMS_Web/target/mac.sh
+++ AutoTest_CMS_Web/target/mac.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 java -jar /Users/tuzi/AndroidStudioProjects/AutoTest/AutoTest_CMS_Web/target/resin-pro-3.1.12/lib/resin.jar -conf /Users/tuzi/AndroidStudioProjects/AutoTest/AutoTest_CMS_Web/target/resin-mac.xml -server AutoTest_CMS_Web  start
