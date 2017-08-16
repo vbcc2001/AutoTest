51ace6f59809278f4f73554c8b9a290d9e873efa
vbcc2001 <vbcc2001@gmail.com>
2017-08-16 18:24:35 +0800

    121
    121

--- AutoTest_CMS_Web/src/main/java/com/xxx/lfs/function/F100006.java
+++ AutoTest_CMS_Web/src/main/java/com/xxx/lfs/function/F100006.java
@@ -7,7 +7,11 @@ import com.xxx.web.function.ResponseParameter;
 import com.xxx.web.jdbc.DBConfigure;
 
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+
+import java.sql.Timestamp;
+import java.util.TimeZone;
 
 /** 添加  */
 public class F100006 extends BaseFunction   {
@@ -24,6 +28,20 @@ public class F100006 extends BaseFunction   {
 
 		String sql ="SELECT * FROM t_count where phone = ? ";
 		List<DataRow> list = this.getNewJdbcTemplate().query(sql,new String[]{phone});
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		//df.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+		for(DataRow dataRow:list){
+			Timestamp sunDate = (Timestamp) dataRow.get("sun_update_time");
+			Timestamp douDate = (Timestamp) dataRow.get("dou_update_time");
+			if(sunDate!=null){
+				String sunDateString = df.format(sunDate.getTime()+1000*60*60*8);
+				dataRow.set("sun_update_time",sunDateString);
+			}
+			if(douDate!=null){
+				String douDateString = df.format(douDate.getTime()+1000*60*60*8);
+				dataRow.set("dou_update_time",douDateString);
+			}
+		}
 		return list;
 	}
 	public static void main(String arg[] ) throws Exception{
