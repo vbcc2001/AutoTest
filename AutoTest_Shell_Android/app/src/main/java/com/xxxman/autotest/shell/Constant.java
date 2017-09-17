7dd3340333767fa4eba2d1042606fb0bc5390e08
vbcc2001 <vbcc2001@gmail.com>
2017-09-17 22:20:37 +0800

    197
    197

--- AutoTest_Shell_Android/app/src/main/java/com/xxxman/autotest/shell/Constant.java
+++ AutoTest_Shell_Android/app/src/main/java/com/xxxman/autotest/shell/Constant.java
@@ -13,21 +13,21 @@ public class Constant {
 
     public static final String REG_URL = "http://reg.zbqhb.com:3000/action/lfs/action/FunctionAction";
 
-//    public static final String URL = "http://ite.zbqhb.com:3000/action/lfs/action/FunctionAction";
-    public static final String URL = "http://hd.zbqhb.com:3000/action/lfs/action/FunctionAction";
+    public static final String URL = "http://ite.zbqhb.com:3000/action/lfs/action/FunctionAction";
+//    public static final String URL = "http://hd.zbqhb.com:3000/action/lfs/action/FunctionAction";
 //    public static final String URL = "http://hsm.zbqhb.com:3000/action/lfs/action/FunctionAction";
 //    public static final String URL = "http://ab.zbqhb.com:3000/action/lfs/action/FunctionAction";
 //
     public static final boolean IS_4X = true;
 //    public static final boolean IS_4X = false;
 
-//    public static final String TAG = "ite";
-    public static final String TAG = "hd";
+    public static final String TAG = "ite";
+//    public static final String TAG = "hd";
 //    public static final String TAG = "hsm";
 //    public static final String TAG = "ab";
 
-//    public static final String LO_CITY = "深圳";
-    public static final String LO_CITY = "揭阳";
+    public static final String LO_CITY = "深圳";
+//    public static final String LO_CITY = "揭阳";
 
     public static final boolean IS_HSM = false;
 //    public static final boolean IS_HSM = true;
