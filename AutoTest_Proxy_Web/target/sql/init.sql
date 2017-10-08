0a341feefab2954ed02b7484f3f5fd854468f72d
vbcc2001 <vbcc2001@gmail.com>
2017-10-08 16:54:27 +0800

    226

--- AutoTest_Proxy_Web/target/sql/init.sql
+++ AutoTest_Proxy_Web/target/sql/init.sql
@@ -1,11 +1,10 @@
-    CREATE TABLE t_hongbao_info (
-      id int(10) NOT NULL AUTO_INCREMENT COMMENT '编号',
-      uid varchar(50)  NULL  COMMENT '花椒号',
-      update_time  timestamp  NULL   COMMENT '更新时间',
-      state varchar(100) DEFAULT NULL COMMENT '账号状态',
-      PRIMARY KEY (id)
-    );
-
-
+CREATE TABLE t_hongbao_info (
+  id int(10) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  uid varchar(50)  NULL  COMMENT '花椒号',
+  update_time  timestamp  NULL   COMMENT '更新时间',
+  state varchar(100) DEFAULT NULL COMMENT '账号状态',
+  PRIMARY KEY (id)
+);
 
 select * from t_hongbao_info
+--delete from t_hongbao_info
