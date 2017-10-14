c1b26f9333a94b71967b0f81964f99ed33008372
vbcc2001 <vbcc2001@gmail.com>
2017-10-14 20:33:14 +0800

    333

--- AutoTest_Proxy_Web/target/resinctl.sh
+++ AutoTest_Proxy_Web/target/resinctl.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 ulimit -n 357353
-PROJECT_PATH=/srv/AutoTest/AutoTest_Proxy_Web/
+PROJECT_PATH=/srv/AutoTest/AutoTest_Proxy_Web
 APP_NAME=AutoTest_Proxy_Web
-APP_HOME=$PROJECT_PATH/target/
+APP_HOME=$PROJECT_PATH/target
 JAVA_HOME=$PROJECT_PATH/target/jdk1.8.0_144/jre
 JAVA=$JAVA_HOME/bin/java
 export JAVA_HOME
 RESIN_HOME=$PROJECT_PATH/target/resin-pro-3.1.12
-RESIN_CONF=$PROJECT_PATH/target/resin.xml
-RESIN=$RESIN_HOME/lib/resin-linux.jar
+RESIN_CONF=$PROJECT_PATH/target/resin-linux.xml
+RESIN=$RESIN_HOME/lib/resin.jar
 USER=root
 JAVA_CONF="-Dfile.encoding=UTF-8 -Dapp.home=$APP_HOME -Dapp.name=$APP_NAME -jar $RESIN  -resin-home $RESIN_HOME -conf $RESIN_CONF  -server $APP_NAME "
 case "$1" in
